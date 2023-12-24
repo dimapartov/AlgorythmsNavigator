68b89b70c5b34570b254ecf8486d18c936485d2a
dimapartov <mr_kubarev@mail.ru>
2023-12-24 18:54:57 +0300

    Add getter and setter for route

--- src/main/java/Route.java
+++ src/main/java/Route.java
@@ -9,6 +9,46 @@ public class Route {
     private Boolean isFavourite;
     private List<String> locationPoints;
 
+    public Route(String id, Double distance, Integer popularity, Boolean isFavourite, List<String> locationPoints) {
+        this.id = id;
+        this.distance = distance;
+        this.popularity = popularity;
+        this.isFavourite = isFavourite;
+        this.locationPoints = locationPoints;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public Double getDistance() {
+        return distance;
+    }
+    public Integer getPopularity() {
+        return popularity;
+    }
+    public Boolean getFavourite() {
+        return isFavourite;
+    }
+    public List<String> getLocationPoints() {
+        return locationPoints;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setDistance(Double distance) {
+        this.distance = distance;
+    }
+    public void setPopularity(Integer popularity) {
+        this.popularity = popularity;
+    }
+    public void setFavourite(Boolean favourite) {
+        isFavourite = favourite;
+    }
+    public void setLocationPoints(List<String> locationPoints) {
+        this.locationPoints = locationPoints;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
